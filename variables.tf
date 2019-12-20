c233ebd54016c2fea1377b6c345920d18bc14d72
Ryan <ryan.fee625@gmail.com>
2019-12-20 12:58:40 -0500

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "scalr_azurerm_subscription_id" {}
+
+variable "scalr_azurerm_client_id" {}
+
+variable "scalr_azurerm_client_secret" {}
+
+variable "scalr_azurerm_tenant_id" {}
+
+variable "region" {}
+
+variable "resourcegroup" {}
+
+variable "aql__server_name" {}
+
+variable "server_username" {}
+
+variable "server_password" {}
+
+variable "db_name" {}
